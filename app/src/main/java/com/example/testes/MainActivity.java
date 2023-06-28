78dc8ac0e75bc96f31dcc3389395e963d7b53415
Vitor Manoel Alves de Andrade <vitorgml21@gmail.com>
2023-06-28 18:40:06 -0300

    att

---
+++ app/src/main/java/com/example/testes/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.testes;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawerLayout);
+    }
+
+    public void openSidebar(View view) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+}
