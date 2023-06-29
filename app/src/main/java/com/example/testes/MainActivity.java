cd9ca5590e5b392662a4d26221d29387287eeab5
GABRIEL DOS REIS TABARELLI <gabriel_tabarelli@estudante.sc.senai.br>
2023-06-28 21:45:29 -0300

    Cadastro e Login

--- app/src/main/java/com/example/testes/MainActivity.java
+++ app/src/main/java/com/example/testes/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.testes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
@@ -14,6 +16,8 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
+import com.example.testes.login.LoginActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
@@ -23,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         drawerLayout = findViewById(R.id.drawerLayout);
+
+        Button button = findViewById(R.id.buttonTeste);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 
     public void openSidebar(View view) {
---
+++ app/src/main/java/com/example/testes/cadastro/CadastroActivity.java
@@ -0,0 +1,17 @@
+package com.example.testes.cadastro;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.testes.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+
+}
