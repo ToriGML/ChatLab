e2e85c36b1c3db9a1e58c74ed303f992d6ca8f46
Vitor Manoel Alves de Andrade <vitorgml21@gmail.com>
2023-06-28 21:45:54 -0300

    att

--- app/src/main/java/com/example/testes/MainActivity.java
+++ app/src/main/java/com/example/testes/MainActivity.java
@@ -1,28 +1,199 @@
 package com.example.testes;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView search;
+    ImageView settings;
     private DrawerLayout drawerLayout;
+    private Dialog dialog;
+    private LinearLayout dialogRootLayout;
+    private float startY;
+    private float initialY;
+    private boolean isDragging = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         drawerLayout = findViewById(R.id.drawerLayout);
+        settings = findViewById(R.id.settings);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogSettings();
+            }
+        });
+        search = findViewById(R.id.search);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogSearch();
+            }
+        });
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void showDialogSearch() {
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottom_sheet_layout_search);
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, 1600);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+        dialogRootLayout = dialog.findViewById(R.id.search_root_layout);
+
+        dialogRootLayout.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                float y = event.getRawY();
+
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        startY = y;
+                        initialY = dialogRootLayout.getY();
+                        isDragging = false;
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        float deltaY = y - startY;
+
+                        if (Math.abs(deltaY) > 10) {
+                            isDragging = true;
+
+                            float newY = initialY + deltaY;
+                            if (newY >= 0 && newY <= dialogRootLayout.getHeight()) {
+                                dialogRootLayout.setY(newY);
+                            }
+                        }
+
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if (isDragging) {
+                            isDragging = false;
+                            float finalY = dialogRootLayout.getY();
+                            float threshold = dialogRootLayout.getHeight() / 6;
+
+                            if (finalY < threshold) {
+                                // Abre o bottom sheet completamente
+                                dialogRootLayout.setY(0);
+                            } else {
+                                // Fecha o bottom sheet
+                                dialogRootLayout.animate()
+                                        .translationY(dialogRootLayout.getHeight())
+                                        .setInterpolator(new AccelerateInterpolator())
+                                        .setDuration(300)
+                                        .withEndAction(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                // Define o posicionamento correto do diálogo antes de descartá-lo
+                                                dialog.getWindow().setAttributes(dialog.getWindow().getAttributes());
+                                                dialog.dismiss();
+                                            }
+                                        })
+                                        .start();
+                            }
+                        }
+                        break;
+                }
+
+                return true;
+            }
+        });
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void showDialogSettings() {
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottom_sheet_layout_settings);
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, 1600);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+        dialogRootLayout = dialog.findViewById(R.id.settings_root_layout);
+
+        dialogRootLayout.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                float y = event.getRawY();
+
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        startY = y;
+                        initialY = dialogRootLayout.getY();
+                        isDragging = false;
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        float deltaY = y - startY;
+
+                        if (Math.abs(deltaY) > 10) {
+                            isDragging = true;
+
+                            float newY = initialY + deltaY;
+                            if (newY >= 0 && newY <= dialogRootLayout.getHeight()) {
+                                dialogRootLayout.setY(newY);
+                            }
+                        }
+
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if (isDragging) {
+                            isDragging = false;
+                            float finalY = dialogRootLayout.getY();
+                            float threshold = dialogRootLayout.getHeight() / 6;
+
+                            if (finalY < threshold) {
+                                // Abre o bottom sheet completamente
+                                dialogRootLayout.setY(0);
+                            } else {
+                                // Fecha o bottom sheet
+                                dialogRootLayout.animate()
+                                        .translationY(dialogRootLayout.getHeight())
+                                        .setInterpolator(new AccelerateInterpolator())
+                                        .setDuration(300)
+                                        .withEndAction(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                // Define o posicionamento correto do diálogo antes de descartá-lo
+                                                dialog.getWindow().setAttributes(dialog.getWindow().getAttributes());
+                                                dialog.dismiss();
+                                            }
+                                        })
+                                        .start();
+                            }
+                        }
+                        break;
+                }
+
+                return true;
+            }
+        });
     }
 
     public void openSidebar(View view) {
