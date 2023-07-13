aa019d5276f1ac923af2818309202ba6d7a7d500
GABRIEL DOS REIS TABARELLI <gabriel_tabarelli@estudante.sc.senai.br>
2023-07-12 21:42:37 -0300

    Fazendo a parte de login com o firebase

--- app/src/main/java/com/example/testes/cadastro/CadastroActivity.java
+++ app/src/main/java/com/example/testes/cadastro/CadastroActivity.java
@@ -1,30 +1,33 @@
 package com.example.testes.cadastro;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.testes.R;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class CadastroActivity extends AppCompatActivity {
 
-    private TextInputLayout senhaLayout;
-    private TextInputLayout confirmarSenhaLayout;
     private TextInputEditText senhaText;
     private TextInputEditText confirmarSenhaText;
+    private TextInputEditText loginText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
 
-        senhaLayout = findViewById(R.id.textInputLayoutSenha);
-        confirmarSenhaLayout = findViewById(R.id.textInputLayoutConfirmarSenha);
         senhaText = findViewById(R.id.textInpuTextSenha);
         confirmarSenhaText = findViewById(R.id.textInpuTextConfirmarSenha);
+        loginText = findViewById(R.id.textInputTextEmail);
 
         Button cadastrarButton = findViewById(R.id.botaoCadastrar);
         Button voltarEntrarButton = findViewById(R.id.botaoVoltarEntrar);
@@ -32,16 +35,26 @@ public class CadastroActivity extends AppCompatActivity {
         cadastrarButton.setOnClickListener(view -> {
             String senha = senhaText.getText().toString();
             String confirmarSenha = confirmarSenhaText.getText().toString();
+            String login = loginText.getText().toString();
+            if (login.isEmpty() || senha.isEmpty() || confirmarSenha.isEmpty()) {
+                showSnackbar(view, "Há dados não preenchidos");
+                return;
+            }
             if (!senha.equals(confirmarSenha)) {
-                confirmarSenhaLayout.setError("As senha não conferem");
-            } else {
-                confirmarSenhaLayout.setError(null);
+                showSnackbar(view, "As senha não conferem");
+                return;
             }
         });
 
         voltarEntrarButton.setOnClickListener(view -> {
             finish();
         });
+    }
+
+    private void showSnackbar(View view, String text) {
+        Snackbar snackbar = Snackbar.make(view, text, Snackbar.LENGTH_SHORT);
+        snackbar.setBackgroundTint(Color.RED);
+        snackbar.show();
     }
 
 }
--- app/src/main/java/com/example/testes/login/LoginActivity.java
+++ app/src/main/java/com/example/testes/login/LoginActivity.java
@@ -1,21 +1,36 @@
 package com.example.testes.login;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.Button;
+import static android.content.ContentValues.TAG;
 
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.testes.MainActivity;
 import com.example.testes.R;
 import com.example.testes.cadastro.CadastroActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
 
         Button botaoCadastro = findViewById(R.id.botaoCadastro);
         botaoCadastro.setOnClickListener(view -> {
@@ -25,9 +40,39 @@ public class LoginActivity extends AppCompatActivity {
 
         Button botaoEntrar = findViewById(R.id.botaoEntrar);
         botaoEntrar.setOnClickListener(view -> {
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
+//            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//            startActivity(intent);
+            singin("gmail@gmail.com", "123");
         });
+
+    }
+
+    public void singin(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "signInWithCustomToken:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    // updateUI(user);
+                } else {
+                    Log.w(TAG, "signInWithCustomToken:failure", task.getException());
+                    Toast.makeText(getApplicationContext(), "Erro", Toast.LENGTH_SHORT).show();
+                    //updateUI(null);
+                }
+            }
+        });
+    }
+
+    public void singout() {
+        FirebaseAuth.getInstance().signOut();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
     }
 
 }
