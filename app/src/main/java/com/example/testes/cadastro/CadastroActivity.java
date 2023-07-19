c18a4bc5503854eaacbd9642ba098305bb185b80
GABRIEL DOS REIS TABARELLI <gabriel_tabarelli@estudante.sc.senai.br>
2023-07-19 19:29:30 -0300

    Fazendo a autenticação com firebase

--- app/src/main/java/com/example/testes/cadastro/CadastroActivity.java
+++ app/src/main/java/com/example/testes/cadastro/CadastroActivity.java
@@ -1,9 +1,13 @@
 package com.example.testes.cadastro;
 
+import static android.content.ContentValues.TAG;
+
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,10 +24,14 @@ public class CadastroActivity extends AppCompatActivity {
     private TextInputEditText confirmarSenhaText;
     private TextInputEditText loginText;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        mAuth = FirebaseAuth.getInstance();
 
         senhaText = findViewById(R.id.textInpuTextSenha);
         confirmarSenhaText = findViewById(R.id.textInpuTextConfirmarSenha);
@@ -35,8 +43,8 @@ public class CadastroActivity extends AppCompatActivity {
         cadastrarButton.setOnClickListener(view -> {
             String senha = senhaText.getText().toString();
             String confirmarSenha = confirmarSenhaText.getText().toString();
-            String login = loginText.getText().toString();
-            if (login.isEmpty() || senha.isEmpty() || confirmarSenha.isEmpty()) {
+            String email = loginText.getText().toString();
+            if (email.isEmpty() || senha.isEmpty() || confirmarSenha.isEmpty()) {
                 showSnackbar(view, "Há dados não preenchidos");
                 return;
             }
@@ -44,10 +52,23 @@ public class CadastroActivity extends AppCompatActivity {
                 showSnackbar(view, "As senha não conferem");
                 return;
             }
+            cadastrar(email, senha);
         });
 
         voltarEntrarButton.setOnClickListener(view -> {
             finish();
+        });
+    }
+
+    private void cadastrar(String email, String senha) {
+        mAuth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                Log.d(TAG, "signInWithCustomToken:success");
+                FirebaseUser user = mAuth.getCurrentUser();
+                finish();
+            } else {
+                Toast.makeText(getApplicationContext(), "Falha ao fazer o cadastro", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 
--- app/src/main/java/com/example/testes/login/LoginActivity.java
+++ app/src/main/java/com/example/testes/login/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -17,6 +18,7 @@ import com.example.testes.R;
 import com.example.testes.cadastro.CadastroActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -32,34 +34,30 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        TextInputEditText email = findViewById(R.id.txtEmail);
+        TextInputEditText senha = findViewById(R.id.txtSenha);
+
         Button botaoCadastro = findViewById(R.id.botaoCadastro);
         botaoCadastro.setOnClickListener(view -> {
-            Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
+            Intent intent = new Intent(getApplicationContext(), CadastroActivity.class);
             startActivity(intent);
         });
 
         Button botaoEntrar = findViewById(R.id.botaoEntrar);
         botaoEntrar.setOnClickListener(view -> {
-//            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//            startActivity(intent);
-            singin("gmail@gmail.com", "123");
+            singin(email.getText().toString(), senha.getText().toString());
         });
 
     }
 
-    public void singin(String email, String password) {
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Log.d(TAG, "signInWithCustomToken:success");
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    // updateUI(user);
-                } else {
-                    Log.w(TAG, "signInWithCustomToken:failure", task.getException());
-                    Toast.makeText(getApplicationContext(), "Erro", Toast.LENGTH_SHORT).show();
-                    //updateUI(null);
-                }
+    public void singin(String email, String senha) {
+        mAuth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                Log.d(TAG, "signInWithCustomToken:success");
+                entrar();
+            } else {
+                Log.w(TAG, "signInWithCustomToken:failure", task.getException());
+                Toast.makeText(getApplicationContext(), "Falha ao fazer o login", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -72,7 +70,15 @@ public class LoginActivity extends AppCompatActivity {
     public void onStart() {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
-//        updateUI(currentUser);
+        if(currentUser != null){
+            entrar();
+        }
+    }
+
+    public void entrar() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
